b6932c48410752c59bde6f75b99fb76ee93d22b6
Simone Trubian <simone.trubian@sainsburys.co.uk>
2021-12-01 15:43:04 +0000

    Stubbed CLI command

---
+++ cmd/sb/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	var sbCmd = &cobra.Command{
+		Use:   "sb",
+		Short: "Simone's blockchain command line interface",
+		Run:   func(cmd *cobra.Command, args []string) {},
+	}
+
+	err := sbCmd.Execute()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
