6552b230365bd7f715a34c1bf5fd8644db515aea
Simone Trubian <simone.trubian@sainsburys.co.uk>
2021-12-01 15:56:07 +0000

    Added version command

--- cmd/sb/main.go
+++ cmd/sb/main.go
@@ -14,6 +14,8 @@ func main() {
 		Run:   func(cmd *cobra.Command, args []string) {},
 	}
 
+	sbCmd.AddCommand(versionCmd)
+
 	err := sbCmd.Execute()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
